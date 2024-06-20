1165c9493c4afca92055dae9aaac434a3c17f306
ishanjoshi2000 <38184604+ishanjoshi2000@users.noreply.github.com>
2024-06-20 16:17:42 +0545

    server setup

--- scripts/script.js
+++ scripts/script.js
@@ -3,295 +3,295 @@ const data={
 
         {
             "name": "Margherita Pizza",
-            "price": 10.99,
+            "price": 40,
             "image": "../images/food/img1.jpg",
             "category": "Pizza"
             },
             {
             "name": "Chicken Alfredo Pasta",
-            "price": 12.99,
+            "price": 10,
             "image": "../images/food/img2.jpg",
             "category": "Pasta"
             },
             {
             "name": "Grilled Chicken Sandwich",
-            "price": 8.99,
+            "price": 50,
             "image": "../images/food/img3.jpg",
             "category": "Sandwiches"
             },
             {
             "name": "Caesar Salad",
-            "price": 7.49,
+            "price": 50,
             "image": "../images/food/img4.jpg",
             "category": "Salads"
             },
             {
             "name": "Cheeseburger",
-            "price": 9.99,
+            "price": 100,
             "image": "../images/food/img5.jpg",
             "category": "Burgers"
             },
             {
             "name": "Fish and Chips",
-            "price": 11.99,
+            "price": 500,
             "image": "../images/food/img6.jpg",
             "category": "Seafood"
             },
             {
             "name": "Beef Tacos",
-            "price": 10.49,
+            "price": 700,
             "image": "../images/food/img7.jpg",
             "category": "Tacos"
             },
             {
             "name": "Vegetarian Sushi Roll",
-            "price": 9.99,
+            "price": 90,
             "image": "../images/food/img8.jpg",
             "category": "Sushi"
             },
             {
             "name": "Chicken Wings",
-            "price": 8.99,
+            "price": 180,
             "image": "../images/food/img9.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Chocolate Brownie Sundae",
-            "price": 6.99,
+            "price": 600,
             "image": "../images/food/img10.jpg",
             "category": "Desserts"
             },
             {
             "name": "Mushroom Risotto",
-            "price": 11.99,
+            "price": 110,
             "image": "../images/food/img11.jpg",
             "category": "Risotto"
             },
             {
             "name": "BBQ Ribs",
-            "price": 14.99,
+            "price": 1400,
             "image": "../images/food/img12.jpg",
             "category": "BBQ"
             },
             {
             "name": "Philly Cheesesteak",
-            "price": 10.99,
+            "price": 1000,
             "image": "../images/food/img13.jpg",
             "category": "Sandwiches"
             },
             {
             "name": "Shrimp Scampi",
-            "price": 13.49,
+            "price": 130,
             "image": "../images/food/img14.jpg",
             "category": "Seafood"
             },
             {
             "name": "Vegetable Stir-Fry",
-            "price": 9.99,
+            "price": 70,
             "image": "../images/food/img15.jpg",
             "category": "Stir Fry"
             },
             {
             "name": "Chicken Quesadilla",
-            "price": 10.49,
+            "price": 100,
             "image": "../images/food/img16.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Caprese Salad",
-            "price": 8.49,
+            "price": 800,
             "image": "../images/food/img17.jpg",
             "category": "Salads"
             },
             {
             "name": "Eggplant Parmesan",
-            "price": 11.99,
+            "price": 1100,
             "image": "../images/food/img18.jpg",
             "category": "Vegetarian"
             },
             {
             "name": "Chicken Caesar Wrap",
-            "price": 9.99,
+            "price": 900,
             "image": "../images/food/img19.jpg",
             "category": "Wraps"
             },
             {
             "name": "Cheese Plate",
-            "price": 13.99,
+            "price": 130,
             "image": "../images/food/img20.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Vegetable Soup",
-            "price": 5.99,
+            "price": 500,
             "image": "../images/food/img21.jpg",
             "category": "Soups"
             },
             {
             "name": "Tiramisu",
-            "price": 7.99,
+            "price": 700,
             "image": "../images/food/img22.jpg",
             "category": "Desserts"
             },
             {
             "name": "Beef Stroganoff",
-            "price": 12.99,
+            "price": 1200,
             "image": "../images/food/img23.jpg",
             "category": "Entrees"
             },
             {
             "name": "Pad Thai",
-            "price": 10.99,
+            "price": 1000,
             "image": "../images/food/img24.jpg",
             "category": "Noodles"
             },
             {
             "name": "Cobb Salad",
-            "price": 9.49,
+            "price": 900,
             "image": "../images/food/img25.jpg",
             "category": "Salads"
             },
             {
             "name": "Vegetarian Pizza",
-            "price": 11.99,
+            "price": 1100,
             "image": "../images/food/img26.jpg",
             "category": "Pizza"
             },
             {
             "name": "Lobster Bisque",
-            "price": 8.99,
+            "price": 800,
             "image": "../images/food/img1.jpg",
             "category": "Soups"
             },
             {
             "name": "Tuna Tartare",
-            "price": 14.99,
+            "price": 1400,
             "image": "../images/food/img2.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Beef Burrito",
-            "price": 11.49,
+            "price": 1100,
             "image": "../images/food/img3.jpg",
             "category": "Burritos"
             },
             {
             "name": "Chicken Fried Rice",
-            "price": 9.99,
+            "price": 900,
             "image": "../images/food/img4.jpg",
             "category": "Rice Dishes"
             },
             {
             "name": "Key Lime Pie",
-            "price": 6.99,
+            "price": 600,
             "image": "../images/food/img5.jpg",
             "category": "Desserts"
             },
             {
             "name": "Sushi Combo Platter",
-            "price": 16.99,
+            "price": 1600,
             "image": "../images/food/img6.jpg",
             "category": "Sushi"
             },
             {
             "name": "French Onion Soup",
-            "price": 7.99,
+            "price": 700,
             "image": "../images/food/img7.jpg",
             "category": "Soups"
             },
             {
             "name": "Pulled Pork Sandwich",
-            "price": 9.99,
+            "price": 900,
             "image": "../images/food/img8.jpg",
             "category": "Sandwiches"
             },
             {
             "name": "Fettuccine Alfredo",
-            "price": 11.49,
+            "price": 1100,
             "image": "../images/food/img9.jpg",
             "category": "Pasta"
             },
             {
             "name": "Beef Empanadas",
-            "price": 8.99,
+            "price": 800,
             "image": "../images/food/img10.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Veggie Burger",
-            "price": 10.49,
+            "price": 1000,
             "image": "../images/food/img11.jpg",
             "category": "Burgers"
             },
             {
             "name": "Clam Chowder",
-            "price": 8.49,
+            "price": 800,
             "image": "../images/food/img12.jpg",
             "category": "Soups"
             },
             {
             "name": "Fajitas",
-            "price": 13.99,
+            "price": 1300,
             "image": "../images/food/img13.jpg",
             "category": "Mexican"
             },
             {
             "name": "Steak",
-            "price": 19.99,
+            "price": 1900,
             "image": "../images/food/img14.jpg",
             "category": "Entrees"
             },
             {
             "name": "Chicken Tenders",
-            "price": 8.99,
+            "price": 800,
             "image": "../images/food/img15.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Vegetable Tempura",
-            "price": 9.99,
+            "price": 900,
             "image": "../images/food/img16.jpg",
             "category": "Appetizers"
             },
             {
             "name": "Mango Sticky Rice",
-            "price": 6.49,
+            "price": 659,
             "image": "../images/food/img17.jpg",
             "category": "Desserts"
               },
               {
                 "name": "Gourmet Mac and Cheese",
-                "price": 10.99,
+                "price": 1000,
                 "image": "../images/food/img18.jpg",
                 "category": "Mac and Cheese"
               },
               {
                 "name": "California Roll",
-                "price": 9.99,
+                "price": 900,
                 "image": "../images/food/img19.jpg",
                 "category": "Sushi"
               },
               {
                 "name": "Lemon Pepper Chicken",
-                "price": 12.49,
+                "price": 1200,
                 "image": "../images/food/img20.jpg",
                 "category": "Entrees"
               },
               {
                 "name": "Vegetarian Wrap",
-                "price": 8.99,
+                "price": 800,
                 "image": "../images/food/img21.jpg",
                 "category": "Wraps"
               },
               {
                 "name": "Chicken Shawarma",
-                "price": 10.99,
+                "price": 1000,
                 "image": "../images/food/img22.jpg",
                 "category": "Middle Eastern"
               },
               {
                 "name": "Stuffed Mushrooms",
-                "price": 7.99,
+                "price": 700,
                 "image": "../images/food/img23.jpg",
                 "category": "Appetizers"
               }
@@ -366,6 +366,7 @@ function updateCart() {
         `;
         cartBody.appendChild(cartRow);
         serialNumber++;
+        updatebilltotal();
     });
 }
 
@@ -393,7 +394,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-
+function updatebilltotal(){
+  grandtotal=0;
+  for (let i = 0; i < cart.length; i++) {
+    grandtotal = grandtotal + cart[i].total;
+}
+  document.getElementById("billtotal").value=grandtotal.toFixed();
+}
 
 
 
